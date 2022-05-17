dba31dfc211ad9b905aa0330f9985e4d37a906b0
Ilya Petrukhin <Tvules@mail.ru>
2022-05-17 20:27:57 +0400

    Custom page 404, 403.

--- yatube/posts/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.paginator import Paginator
-
-
-def _get_page_obj(request, object_list, per_page):
-    """Возвращает объект страницы"""
-    paginator = Paginator(object_list, per_page)
-    page_number = request.GET.get('page')
-    return paginator.get_page(page_number)
